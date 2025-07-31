cf9075d08a8b64ca2ca0615ed5efea22839308e5
ShionTerunaga <sion2769@gmail.com>
2025-08-01 03:59:57 +0900

    feat: create card

--- example/next/app/v-15.3/src/components/layout/card/card.tsx
+++ example/next/app/v-15.3/src/components/layout/card/card.tsx
@@ -1,3 +1,54 @@
-export function Card() {
-    return <></>;
+import { CheckerProps } from "@/shared/types/object";
+import Image, { StaticImageData } from "next/image";
+import { CSSProperties } from "react";
+
+interface Props {
+    title: string;
+    src: StaticImageData | string;
+    alt: string;
+    boxHeight: number;
+    srcWidth: number;
+    srcHeight: number;
+}
+
+export function Card<T extends Props>(
+    props: CheckerProps<T, Props, "different card props">
+) {
+    const { title, src, alt, srcWidth, srcHeight, boxHeight } = props;
+
+    const cardStyle: CSSProperties = {
+        width: srcWidth,
+        height: boxHeight,
+        border: "1px solid #ccc",
+        borderRadius: "10px",
+        overflow: "hidden",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        textAlign: "center",
+        margin: "1rem"
+    };
+
+    const imageStyle: CSSProperties = {
+        objectFit: "cover"
+    };
+
+    const nameStyle: CSSProperties = {
+        padding: "1rem",
+        fontSize: "1.2rem",
+        fontWeight: "bold",
+        color: "#333",
+        margin: 0
+    };
+
+    return (
+        <div style={cardStyle}>
+            <Image
+                src={src}
+                alt={alt}
+                style={imageStyle}
+                width={srcWidth}
+                height={srcHeight}
+            />
+            <p style={nameStyle}>{title}</p>
+        </div>
+    );
 }
