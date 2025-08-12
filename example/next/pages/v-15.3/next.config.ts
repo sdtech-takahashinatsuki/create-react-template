c11a96ec2f9cf4f12eb1b0855934a78e48508f7e
ShionTerunaga <sion2769@gmail.com>
2025-08-13 03:16:32 +0900

    feat: add pages router

--- example/next/pages/v-15.3/next.config.ts
+++ example/next/pages/v-15.3/next.config.ts
@@ -1,8 +1,14 @@
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 import type { NextConfig } from "next";
 
+const withVanillaExtract = createVanillaExtractPlugin();
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    /* config options here */
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [new URL("https://ik.imagekit.io/**")]
+    }
 };
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
---
+++ example/next/pages/v-15.3/src/utils/result.ts
@@ -0,0 +1,30 @@
+export const RESULT_OK = "ok" as const;
+export const RESULT_NG = "ng" as const;
+
+interface OK<T> {
+    readonly kind: typeof RESULT_OK;
+    value: T;
+}
+
+interface NG<E> {
+    readonly kind: typeof RESULT_NG;
+    err: E;
+}
+
+export type Result<T, E> = OK<T> | NG<E>;
+
+export const createResult = {
+    ok: <T>(value: T): Result<T, never> => {
+        return {
+            kind: RESULT_OK,
+            value
+        };
+    },
+
+    ng: <E>(err: E): Result<never, E> => {
+        return {
+            kind: RESULT_NG,
+            err
+        };
+    }
+};
