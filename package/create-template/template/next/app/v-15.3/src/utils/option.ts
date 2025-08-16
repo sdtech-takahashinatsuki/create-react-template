7e6d3de3b429ee5c5e42082489f3e1c29169c197
ShionTerunaga <sion2769@gmail.com>
2025-08-16 15:46:29 +0900

    fix: move directory

--- package/create-template/template/next/app/v-15.3/src/utils/option.ts
+++ package/create-template/template/next/app/v-15.3/src/utils/option.ts
@@ -2,26 +2,26 @@ export const OPTION_SOME = "some" as const;
 export const OPTION_NONE = "none" as const;
 
 interface Some<T> {
-  readonly kind: typeof OPTION_SOME;
-  value: T;
+    readonly kind: typeof OPTION_SOME;
+    value: T;
 }
 
 interface None {
-  readonly kind: typeof OPTION_NONE;
+    readonly kind: typeof OPTION_NONE;
 }
 
 export type Option<T> = Some<T> | None;
 
 export const createOption = {
-  some: <T>(value: T): Option<T> => {
-    return {
-      kind: OPTION_SOME,
-      value,
-    };
-  },
-  none: () => {
-    return {
-      kind: OPTION_NONE,
-    };
-  },
+    some: <T>(value: T): Option<T> => {
+        return {
+            kind: OPTION_SOME,
+            value
+        };
+    },
+    none: () => {
+        return {
+            kind: OPTION_NONE
+        };
+    }
 };
