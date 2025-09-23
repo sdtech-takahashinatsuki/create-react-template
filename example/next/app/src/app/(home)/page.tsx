a9db83e9054c12c1b1a6919e84833617733a5695
ShionTerunaga <sion2769@gmail.com>
2025-09-23 22:57:21 +0900

    fix: delete dynamic router

--- example/next/app/src/app/(home)/page.tsx
+++ example/next/app/src/app/(home)/page.tsx
@@ -19,10 +19,6 @@ export default function Home() {
 
             <div>
                 <Link href="/cache-potter">{ja.app.home.toCachePotter}</Link>
-                <br />
-                <Link href="/single-dynamic-potter">
-                    {ja.app.home.toSingleDynamicPotter}
-                </Link>
             </div>
         </main>
     );
--- example/next/app/src/features/harry-potter/index.ts
+++ example/next/app/src/features/harry-potter/index.ts
@@ -1,6 +1,3 @@
 export * from "./model/model-res";
 export * from "./model/model-view";
 export * from "./service/get-character";
-
-export * from "./hooks/single-page-characters";
-export * from "./hooks/characters.type";
