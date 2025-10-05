63238596910ad46f0ac7c1666db37b102f5c7d19
ShionTerunaga <sion2769@gmail.com>
2025-10-06 02:07:45 +0900

    feat: add test

--- example/tanstack-router/src/__test__/utils/error/http.test.ts
+++ example/tanstack-router/src/__test__/utils/error/http.test.ts
@@ -1,94 +1,94 @@
-import { createHttpError, HttpError } from "@/utils/error/http";
-import { describe, expect, it } from "vitest";
+import { createHttpError, HttpError } from '@/utils/error/http'
+import { describe, expect, it } from 'vitest'
 
-describe("http", () => {
-    it("httpErrorクラスが正常に作られる", () => {
-        const error = new HttpError({
-            status: 4000,
-            message: "bad request"
-        });
+describe('http', () => {
+  it('httpErrorクラスが正常に作られる', () => {
+    const error = new HttpError({
+      status: 4000,
+      message: 'bad request',
+    })
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(4000);
-        expect(error.message).toBe("bad request");
-    });
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(4000)
+    expect(error.message).toBe('bad request')
+  })
 
-    it("createHttpErrorのnotFoundAPIUrlが正常に作られる", () => {
-        const { notFoundAPIUrl } = createHttpError();
-        const error = notFoundAPIUrl();
+  it('createHttpErrorのnotFoundAPIUrlが正常に作られる', () => {
+    const { notFoundAPIUrl } = createHttpError()
+    const error = notFoundAPIUrl()
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(4040);
-        expect(error.message).toBe("APIのURLが設定されていません");
-    });
-    it("createHttpErrorのreturnNotFoundAPIUrlが正常に作られる", () => {
-        const { returnNotFoundAPIUrl } = createHttpError();
-        const error = returnNotFoundAPIUrl();
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(4040)
+    expect(error.message).toBe('APIのURLが設定されていません')
+  })
+  it('createHttpErrorのreturnNotFoundAPIUrlが正常に作られる', () => {
+    const { returnNotFoundAPIUrl } = createHttpError()
+    const error = returnNotFoundAPIUrl()
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(4041);
-        expect(error.message).toBe("APIのURLが見つかりません");
-    });
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(4041)
+    expect(error.message).toBe('APIのURLが見つかりません')
+  })
 
-    it("createHttpErrorのreturnNoPermissionが正常に作られる", () => {
-        const { returnNoPermission } = createHttpError();
-        const error = returnNoPermission();
+  it('createHttpErrorのreturnNoPermissionが正常に作られる', () => {
+    const { returnNoPermission } = createHttpError()
+    const error = returnNoPermission()
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(4030);
-        expect(error.message).toBe("権限がありません");
-    });
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(4030)
+    expect(error.message).toBe('権限がありません')
+  })
 
-    it("createHttpErrorのreturnBadRequestが正常に作られる", () => {
-        const { returnBadRequest } = createHttpError();
-        const error = returnBadRequest();
+  it('createHttpErrorのreturnBadRequestが正常に作られる', () => {
+    const { returnBadRequest } = createHttpError()
+    const error = returnBadRequest()
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(4000);
-        expect(error.message).toBe("不正なリクエストです");
-    });
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(4000)
+    expect(error.message).toBe('不正なリクエストです')
+  })
 
-    it("createHttpErrorのreturnInternalServerErrorが正常に作られる", () => {
-        const { returnInternalServerError } = createHttpError();
-        const error = returnInternalServerError();
+  it('createHttpErrorのreturnInternalServerErrorが正常に作られる', () => {
+    const { returnInternalServerError } = createHttpError()
+    const error = returnInternalServerError()
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(5001);
-        expect(error.message).toBe("サーバーエラーです");
-    });
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(5001)
+    expect(error.message).toBe('サーバーエラーです')
+  })
 
-    it("createHttpErrorのunknownErrorが正常に作られる", () => {
-        const { unknownError } = createHttpError();
-        const error = unknownError();
+  it('createHttpErrorのunknownErrorが正常に作られる', () => {
+    const { unknownError } = createHttpError()
+    const error = unknownError()
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(9999);
-        expect(error.message).toBe("unknown error");
-    });
-    it("createHttpErrorのschemeErrorが正常に作られる", () => {
-        const { schemeError } = createHttpError();
-        const error = schemeError();
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(9999)
+    expect(error.message).toBe('unknown error')
+  })
+  it('createHttpErrorのschemeErrorが正常に作られる', () => {
+    const { schemeError } = createHttpError()
+    const error = schemeError()
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(5000);
-        expect(error.message).toBe("スキームが間違っています。");
-    });
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(5000)
+    expect(error.message).toBe('スキームが間違っています。')
+  })
 
-    it("createHttpErrorのparseErrorが正常に作られる", () => {
-        const { parseError } = createHttpError();
-        const error = parseError();
+  it('createHttpErrorのparseErrorが正常に作られる', () => {
+    const { parseError } = createHttpError()
+    const error = parseError()
 
-        expect(error).toBeInstanceOf(HttpError);
-        expect(error.type).toBe("httpError");
-        expect(error.status).toBe(8888);
-        expect(error.message).toBe("パースエラーです");
-    });
-});
+    expect(error).toBeInstanceOf(HttpError)
+    expect(error.type).toBe('httpError')
+    expect(error.status).toBe(8888)
+    expect(error.message).toBe('パースエラーです')
+  })
+})
--- example/tanstack-router/src/__test__/utils/result.test.ts
+++ example/tanstack-router/src/__test__/utils/result.test.ts
@@ -1,28 +1,28 @@
-import { createResult } from "@/utils/result";
-import { describe, expect, it } from "vitest";
+import { createResult } from '@/utils/result'
+import { describe, expect, it } from 'vitest'
 
-describe("createResult", () => {
-    it("resultがokだったらvalueに値が入っている", () => {
-        const result = "test";
-        const okResult = createResult.ok(result);
+describe('createResult', () => {
+  it('resultがokだったらvalueに値が入っている', () => {
+    const result = 'test'
+    const okResult = createResult.ok(result)
 
-        if (okResult.kind === "ng") {
-            throw new Error("ng");
-        }
+    if (okResult.kind === 'ng') {
+      throw new Error('ng')
+    }
 
-        expect(okResult.kind).toEqual("ok");
-        expect(okResult.value).toEqual(result);
-    });
+    expect(okResult.kind).toEqual('ok')
+    expect(okResult.value).toEqual(result)
+  })
 
-    it("resultがngだったらerrに値が入っている", () => {
-        const result = "error";
-        const ngResult = createResult.ng(result);
+  it('resultがngだったらerrに値が入っている', () => {
+    const result = 'error'
+    const ngResult = createResult.ng(result)
 
-        if (ngResult.kind === "ok") {
-            throw new Error("ok");
-        }
+    if (ngResult.kind === 'ok') {
+      throw new Error('ok')
+    }
 
-        expect(ngResult.kind).toEqual("ng");
-        expect(ngResult.err).toEqual(result);
-    });
-});
+    expect(ngResult.kind).toEqual('ng')
+    expect(ngResult.err).toEqual(result)
+  })
+})
