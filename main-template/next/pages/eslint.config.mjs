22d757f4081afff5d558c9cc7e93802a36872609
Shion Terunaga <shion.terunaga@ShionnoMacBook-Pro.local>
2025-10-29 16:06:06 +0900

    feat: upgrade nextjs

--- main-template/next/pages/eslint.config.mjs
+++ main-template/next/pages/eslint.config.mjs
@@ -1,46 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname
-});
-
-const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
-    {
-        ignores: [
-            "node_modules/**",
-            ".next/**",
-            "out/**",
-            "build/**",
-            "next-env.d.ts"
-        ]
-    },
-    {
-        languageOptions: {
-            parserOptions: {
-                project: "./tsconfig.json"
-            }
-        },
-        plugins: {
-            "@typescript-eslint": typescriptEslintPlugin
-        },
-        rules: {
-            "@typescript-eslint/strict-boolean-expressions": [
-                "error",
-                {
-                    allowString: false,
-                    allowNumber: false,
-                    allowNullableObject: false
-                }
-            ]
-        }
-    }
-];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
 export default eslintConfig;
